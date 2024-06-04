25934aa76742e779c1fcd070a221fe5822236985
strixer1605 <114455920+strixer1605@users.noreply.github.com>
2024-06-04 14:59:42 -0300

    Add files via upload

---
+++ modulos/crearUsuario.php
@@ -0,0 +1,98 @@
+<?php
+include('conexion.php');
+include("class.phpmailer.php");
+include("class.smtp.php");
+
+// Establecer la zona horaria a Buenos Aires, Argentina
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+// var_dump($_POST);
+
+
+if(isset($_POST['dni'], $_POST['nombre'], $_POST['apellido'], $_POST['email'], $_POST['cargo'])){
+    // Recibir datos del formulario
+    $dni = $_POST['dni'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $cargo = $_POST['cargo'];
+    $fechaCreacion = date('Y-m-d H:i:s');
+    
+    $estado = 0; // Estado inicial desactivado
+    
+    // Verificar si el usuario ya existe por DNI o email
+    $queryVerificar = "SELECT dni FROM usuarios WHERE dni = ? OR gmail = ?";
+    $stmtVerificar = mysqli_prepare($conexion, $queryVerificar);
+    mysqli_stmt_bind_param($stmtVerificar, "is", $dni, $email);
+    mysqli_stmt_execute($stmtVerificar);
+    mysqli_stmt_store_result($stmtVerificar);
+    
+    if(mysqli_stmt_num_rows($stmtVerificar) > 0){
+        echo 'duplicate_dni_or_email';
+        // exit;
+
+    }
+
+    // Insertar el nuevo usuario utilizando una consulta preparada
+    $query = "INSERT INTO usuarios (dni, nombre, apellido, gmail, cargo, estado) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conexion, $query);
+    mysqli_stmt_bind_param($stmt, "issssb", $dni, $nombre, $apellido, $email, $cargo, $estado);
+
+
+
+
+
+
+
+
+
+    if(mysqli_stmt_execute($stmt)){
+        // Generar el token de verificación
+        $token = uniqid();
+
+        // Guardar el token de verificación en la base de datos
+        $queryToken = "INSERT INTO tokens (token, dni, fecha_creacion) VALUES (?, ?, ?)";
+        $stmtToken = mysqli_prepare($conexion, $queryToken);
+        mysqli_stmt_bind_param($stmtToken, "sss", $token, $dni, $fechaCreacion);
+        mysqli_stmt_execute($stmtToken);
+
+        // Configurar y enviar el correo electrónico de verificación
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPDebug  = 0;
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->Port       = 587;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'expositos060@gmail.com';
+        $mail->Password   = 'hkgquaffpschxrky';
+        
+        $mail->IsHTML(true);
+        $mail->setFrom('expositos060@gmail.com', 'Santi');
+        $mail->FromName = '=?UTF-8?B?' . base64_encode("Verificación de Cuenta") . '?=';
+        $mail->Subject = '=?UTF-8?B?' . base64_encode("Verifica tu cuenta") . '?=';
+        
+        $enlace_confirmacion = "http://localhost/jardin/modulos/verificarCuenta.php?token=" . $token;
+        $mensaje = "<p>¡Gracias por registrarte en nuestro sitio!</p>";
+        $mensaje .= "<p>Para activar tu cuenta, por favor haz clic en el siguiente enlace:</p>";
+        $mensaje .= "<p><a href='$enlace_confirmacion'>$enlace_confirmacion</a></p>";
+        $mensaje .= "<p>Si no has solicitado este registro, puedes ignorar este mensaje.</p>";
+        $mail->Body = $mensaje;
+        
+        $mail->addAddress($email);
+
+        if($mail->Send()) {
+            echo 'success';
+        } else {
+            echo "Error: " . $mail->ErrorInfo;
+        }
+    } else {
+        echo 'error';
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($conexion);
+}else {
+    echo "pacho algo";
+}
+?>
---
+++ modulos/recuperarContra.php
@@ -0,0 +1,76 @@
+<?php
+include("class.phpmailer.php");
+include("class.smtp.php");
+include('conexion.php');
+
+$correo = $_POST['data_correo'];
+
+// Verificar si el correo existe en la base de datos
+$query = "SELECT * FROM usuarios WHERE gmail = ?";
+$stmt = $conexion->prepare($query);
+$stmt->bind_param("s", $correo);
+$stmt->execute();
+$resultado = $stmt->get_result();
+
+if ($resultado->num_rows > 0) {
+    $row = $resultado->fetch_assoc();
+    $mail = new PHPMailer();
+
+    try {
+        // Configurar la conexión SMTP
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'expositos060@gmail.com';
+        $mail->Password = 'hkgquaffpschxrky';
+
+        // Configurar el contenido del correo electrónico
+        $mail->IsHTML(true);
+        $mail->setFrom('expositos060@gmail.com', 'Santi');
+        $mail->FromName = "=?UTF-8?B?" . base64_encode("Link para recuperación de contraseña") . "?=";
+        $mail->Subject = "=?UTF-8?B?" . base64_encode("Recuperación de Contraseña") . "?=";
+        
+        // Añadir la cabecera para especificar la codificación de caracteres UTF-8
+        $mail->CharSet = 'UTF-8';
+        
+        // Generar el token único
+        $token = uniqid();
+        
+        // Almacenar el token en la base de datos
+        $insertTokenQuery = "INSERT INTO tokens (token, dni) VALUES (?, ?)";
+        $insertStmt = $conexion->prepare($insertTokenQuery);
+        $insertStmt->bind_param("ss", $token, $row['dni']);
+        $insertStmt->execute();
+
+        // Generar el enlace de confirmación con el token
+        $enlace_confirmacion = "http://localhost/jardin901/modulos/cambiarContra.php?token=" . $token;
+        
+        // Construir el cuerpo del mensaje
+        $mensaje = "<p>Saludos,</p>";
+        $mensaje .= "<p>Para completar el cambio de contraseña, por favor haz clic en el siguiente enlace:</p>";
+        $mensaje .= "<p><a href='$enlace_confirmacion'>$enlace_confirmacion</a></p>";
+        $mensaje .= "<p>Si no has solicitado este cambio, puedes ignorar este mensaje.</p>";
+        $mail->Body = $mensaje;
+        
+        // Agregar el destinatario
+        $mail->addAddress($correo);
+
+        // Enviar el correo electrónico
+        if($mail->send()) {
+            echo json_encode(['status' => 'ok']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'No se pudo enviar el correo']);
+        }
+    } catch (Exception $e) {
+        echo json_encode(['status' => 'error', 'message' => 'Ocurrió un error al enviar el correo']);
+    }
+} else {
+    echo json_encode(['status' => 'not_found', 'message' => 'Correo no encontrado']);
+}
+
+$stmt->close();
+$conexion->close();
+?>
