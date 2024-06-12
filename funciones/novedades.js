514002b340cb56483d0e7316b0c2509c5e254771
JoaquinLorenzo07 <lorenzo.joaquin.et25@gmail.com>
2024-06-12 15:21:24 -0300

    css

--- funciones/novedades.js
+++ funciones/novedades.js
@@ -11,7 +11,7 @@ let items = document.querySelectorAll('.slider .item');
         items[active].style.opacity = 1;
         for(var i = active + 1; i < items.length; i++){
             stt++;
-            items[i].style.transform = `translateX(${150*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.transform = `translateX(${150*stt}px) scale(${1 - 0.2*stt}) perspective(30px) rotateY(-1deg)`;
             items[i].style.zIndex = -stt;
             items[i].style.filter = 'blur(0.2px)';
             items[i].style.opacity = stt > 2 ? 0 : 0.6;
@@ -19,7 +19,7 @@ let items = document.querySelectorAll('.slider .item');
         stt = 0;
         for(var i = active - 1; i >= 0; i--){
             stt++;
-            items[i].style.transform = `translateX(${-150*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.transform = `translateX(${-150*stt}px) scale(${1 - 0.2*stt}) perspective(30px) rotateY(1deg)`;
             items[i].style.zIndex = -stt;
             items[i].style.filter = 'blur(0.2px)';
             items[i].style.opacity = stt > 2 ? 0 : 0.6;
